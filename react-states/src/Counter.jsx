5a27aede6155a21c1f5af0cf97319d8438ad82fb
ankitkatole <katoleankit06@gmail.com>
2024-08-25 18:16:49 +0530

    States

--- react-states/src/Counter.jsx
+++ react-states/src/Counter.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 export default function Counter(){
     let [count,setStateCount]= useState(0);
     let incCount = ()=>{
-        setStateCount(++count);
+        // setStateCount(++count);
+        setStateCount((currCount)=>{
+            return currCount + 1;
+        })
+        setStateCount((currCount)=>{
+            return currCount + 1;
+        })//Calling multiple setState when new Val depend on old valwe do it by Callback since setState is async func
     }
     return(
         <>
