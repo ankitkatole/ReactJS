fc4b73c3fb1ed6dea523219a8cbcb096b5dea22b
ankitkatole <katoleankit06@gmail.com>
2024-10-08 00:42:36 +0530

    Revision

---
+++ reactPractice/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='px-4 py-3'>
+      <form>
+        <input className='px-4 py-3' type='text' placeholder='Enter Name' />
+        <button className="px-4 py-3">Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
