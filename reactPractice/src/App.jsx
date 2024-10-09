10ae2b95d6aaa7baf837ba1e42e0a1847d39aed8
ankitkatole <katoleankit06@gmail.com>
2024-10-09 23:54:26 +0530

    React-Routers

--- reactPractice/src/App.jsx
+++ reactPractice/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Product from './pages/Product';
 
 const App = () => {
   return (
-    <div className='px-4 py-3'>
-      <form>
-        <input className='px-4 py-3' type='text' placeholder='Enter Name' />
-        <button className="px-4 py-3">Submit</button>
-      </form>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
     </div>
   )
 }
